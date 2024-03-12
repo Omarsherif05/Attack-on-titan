65f75b0b6a484d640db2591310f7fc8160f16742
Omar <omarsherifmohammedomar@gmail.com>
2024-03-12 20:34:14 +0200

    .

--- src/game/engine/titans/PureTitan.java
+++ src/game/engine/titans/PureTitan.java
@@ -3,12 +3,9 @@ package game.engine.titans;
 public class PureTitan extends Titan{
 	private final int TITAN_CODE = 	1;
 
-	public PureTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,
-			int resourcesValue, int dangerLevel) {
+	public PureTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,int resourcesValue, int dangerLevel) {
 		super(baseHealth, baseDamage, heightInMeters, distanceFromBase, speed, resourcesValue, dangerLevel);
-		
 	}
-
 	
 	public void move() {
 		
