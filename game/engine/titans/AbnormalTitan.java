2d5cb91ab9e2939f575a24015ae89a0a0ee294ef
Omar <omarsherifmohammedomar@gmail.com>
2024-03-12 21:12:17 +0200

    .

--- game/engine/titans/AbnormalTitan.java
+++ game/engine/titans/AbnormalTitan.java
@@ -1,6 +1,8 @@
 package game.engine.titans;
 
 public class AbnormalTitan extends Titan{
+	private final int TITAN_CODE = 	2;
+
 	public AbnormalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed, int resourcesValue, int dangerLevel){
 		super(baseHealth, baseDamage, heightInMeters, distanceFromBase, speed, resourcesValue, dangerLevel);
 	}
