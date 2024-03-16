8510ff56ee03a6d383ba026d77ac2070161fd721
Omar <omarsherifmohammedomar@gmail.com>
2024-03-16 08:48:01 +0200

    .

--- src/game/engine/dataloader/DataLoader.java
+++ src/game/engine/dataloader/DataLoader.java
@@ -1,25 +1,25 @@
 package game.engine.dataloader;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import game.engine.titans.TitanRegistry;
 import game.engine.weapons.WeaponRegistry;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class DataLoader {
-    
+    // Attributes
     private static final String TITANS_FILE_NAME = "titans.csv";
     private static final String WEAPONS_FILE_NAME = "weapons.csv";
 
-    
-    public static ArrayList<TitanRegistry> loadTitans() {
-        ArrayList<TitanRegistry> titans = new ArrayList<>();
+    // Method to read titans from CSV file and load into HashMap
+    public static HashMap<Integer, TitanRegistry> readTitanRegistry() throws IOException {
+        HashMap<Integer, TitanRegistry> titanMap = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(TITANS_FILE_NAME))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
-               
                 int code = Integer.parseInt(data[0]);
                 int baseHealth = Integer.parseInt(data[1]);
                 int baseDamage = Integer.parseInt(data[2]);
@@ -28,17 +28,15 @@ public class DataLoader {
                 int resourcesValue = Integer.parseInt(data[5]);
                 int dangerLevel = Integer.parseInt(data[6]);
                 TitanRegistry titan = new TitanRegistry(code, baseHealth, baseDamage, heightInMeters, speed, resourcesValue, dangerLevel);
-                titans.add(titan);
+                titanMap.put(code, titan);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return titans;
+        return titanMap;
     }
 
-    
-    public static ArrayList<WeaponRegistry> loadWeapons() {
-        ArrayList<WeaponRegistry> weapons = new ArrayList<>();
+    // Method to read weapons from CSV file and load into HashMap
+    public static HashMap<Integer, WeaponRegistry> readWeaponRegistry() throws IOException {
+        HashMap<Integer, WeaponRegistry> weaponMap = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(WEAPONS_FILE_NAME))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -47,14 +45,21 @@ public class DataLoader {
                 int price = Integer.parseInt(data[1]);
                 int damage = Integer.parseInt(data[2]);
                 String name = data[3];
-                int minRange = Integer.parseInt(data[4]);
-                int maxRange = Integer.parseInt(data[5]);
-                WeaponRegistry weapon = new WeaponRegistry(code, price, damage, name, minRange, maxRange);
-                weapons.add(weapon);
+                int minRange = 0;
+                int maxRange = 0;
+                if (data.length == 6) {
+                    minRange = Integer.parseInt(data[4]);
+                    maxRange = Integer.parseInt(data[5]);
+                }
+                WeaponRegistry weapon;
+                if (data.length == 4) {
+                    weapon = new WeaponRegistry(code, price, damage, name);
+                } else {
+                    weapon = new WeaponRegistry(code, price, damage, name, minRange, maxRange);
+                }
+                weaponMap.put(code, weapon);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return weapons;
+        return weaponMap;
     }
 }
