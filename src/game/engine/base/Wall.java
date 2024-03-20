25fae520ff943a3f846fd6f4ea7bc6d85671acbf
Omar <omarsherifmohammedomar@gmail.com>
2024-03-20 19:41:54 +0200

    .

--- src/game/engine/base/Wall.java
+++ src/game/engine/base/Wall.java
@@ -1,19 +1,25 @@
 package game.engine.base;
 
-import game.engine.interfaces.Attackee;
-
-public class Wall implements Attackee{
-
+public class Wall {
 	
-	
-	public static int baseHealth ;
-	protected int currentHealth;
+    private final int baseHealth;
+    private int currentHealth;
 
+    public Wall(int baseHealth) {
+        this.baseHealth = baseHealth;
+        this.currentHealth = baseHealth;
+    }
 
-	public Wall(int baseHealth) {
-		this.baseHealth = baseHealth;
-		this.currentHealth = baseHealth;
-	}
+    public int getBaseHealth() {
+        return baseHealth;
+    }
 
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
 
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
 }
+
