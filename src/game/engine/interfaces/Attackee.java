5687227d961da933746c5c7c3295ac418f033987
Omar <omarsherifmohammedomar@gmail.com>
2024-04-17 22:02:04 +0200

    .

--- src/game/engine/interfaces/Attackee.java
+++ src/game/engine/interfaces/Attackee.java
@@ -4,4 +4,14 @@ public interface Attackee {
 	void setCurrentHealth(int currentHealth);
 	int getCurrentHealth();
 	int getResourcesValue();
+    default boolean isDefeated() {
+        return getCurrentHealth() <= 0;
+    }
+    default int takeDamage(int damage) {
+    	getCurrentHealth() = getCurrentHealth() - damage;
+        if (isDefeated()) {
+            return getResourcesValue(); 
+        }
+        return 0;
+    }
 }
