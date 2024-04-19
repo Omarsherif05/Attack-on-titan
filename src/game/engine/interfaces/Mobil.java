e0f2e9078befd14de7e673af97bab3f60b54781c
Omar <omarsherifmohammedomar@gmail.com>
2024-04-19 14:44:37 +0200

    .

--- src/game/engine/interfaces/Mobil.java
+++ src/game/engine/interfaces/Mobil.java
@@ -17,5 +17,18 @@ public interface Mobil {
 		 getDistance().moveTowards(setDistance());
          return hasReachedTarget();
         }
+	 
+	 default boolean hasReachedTarget() {
+	        return getDistance() <= 0; // Assuming target is reached when distance is 0 or less
+	    }
+	 
+	 default boolean move() {
+	        int distance = getDistance();
+	        int speed = getSpeed();
 
+	       
+	        setDistance(Math.max(distance - speed, 0));
+
+	        return hasReachedTarget();
+	    }
 }
