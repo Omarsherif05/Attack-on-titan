b4833c2d174d60eb804359e809dec3628e48cc6f
Omar <omarsherifmohammedomar@gmail.com>
2024-04-19 14:52:02 +0200

    .

--- src/game/engine/interfaces/Mobil.java
+++ src/game/engine/interfaces/Mobil.java
@@ -1,34 +1,34 @@
 package game.engine.interfaces;
 
 public interface Mobil {
-	
-	 int getDistance();
-	 void setDistance(int distance);
-	 int getSpeed();
-	 void setSpeed(int speed);
 
-	 /*not finished
-	 default boolean hasReachedTarget(){
-		 return getDistance().equals(setDistance(distance));
-		}
+	int getDistance();
 
-	   
-	 default boolean move(){
-		 getDistance().moveTowards(setDistance());
-         return hasReachedTarget();
-        }
+	void setDistance(int distance);
+
+	int getSpeed();
+
+	void setSpeed(int speed);
+
+	/*
+	 * not finished default boolean hasReachedTarget(){ return
+	 * getDistance().equals(setDistance(distance)); }
+	 * 
+	 * 
+	 * default boolean move(){ getDistance().moveTowards(setDistance()); return
+	 * hasReachedTarget(); }
 	 */
-	 default boolean hasReachedTarget() {
-	        return getDistance() <= 0; 
-	    }
-	 
-	 default boolean move() {
-	        int distance = getDistance();
-	        int speed = getSpeed();
+	
+	default boolean hasReachedTarget() {
+		return getDistance() <= 0;
+	}
 
-	       
-	        setDistance(Math.max(distance - speed, 0));
+	default boolean move() {
+		int distance = getDistance();
+		int speed = getSpeed();
 
-	        return hasReachedTarget();
-	    }
+		setDistance(Math.max(distance - speed, 0));
+
+		return hasReachedTarget();
+	}
 }
