464d1a7bc0c39086247f3cf6a58bb5e290a319b9
mourad2213 <ahmed.mourad338@gmail.com>
2024-04-19 18:17:17 +0200

    x

--- src/game/engine/weapons/SniperCannon.java
+++ src/game/engine/weapons/SniperCannon.java
@@ -1,5 +1,9 @@
 package game.engine.weapons;
 
+import java.util.PriorityQueue;
+
+import game.engine.titans.Titan;
+
 public class SniperCannon extends Weapon {
 	public static final int WEAPON_CODE = 2;
 
@@ -7,9 +11,16 @@ public class SniperCannon extends Weapon {
 		super(baseDamage);
 	}
 
-	
+	int turnAttack(PriorityQueue<Titan> laneTitans){
+		Titan currentTitan =laneTitans.peek();
+		int damage =this.getDamage();
+		currentTitan.takeDamage(damage);
+		if(currentTitan.isDefeated()){
+			laneTitans.remove();
+			return currentTitan.getResourcesValue();
+		}
+		return 0;
+	}
 
     
-
-
 }
