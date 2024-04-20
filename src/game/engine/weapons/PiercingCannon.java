ed8e8cfd9d25398493fa872828f6214c9f59c6ba
Omar <omarsherifmohammedomar@gmail.com>
2024-04-20 14:55:20 +0200

    .

--- src/game/engine/weapons/PiercingCannon.java
+++ src/game/engine/weapons/PiercingCannon.java
@@ -1,22 +1,44 @@
 package game.engine.weapons;
 
+import java.util.Iterator;
 import java.util.PriorityQueue;
 
 import game.engine.titans.Titan;
 
-public class PiercingCannon extends Weapon{
+public class PiercingCannon extends Weapon {
 
 	public static final int WEAPON_CODE = 1;
 
 	public PiercingCannon(int baseDamage) {
 		super(baseDamage);
-		
+
 	}
 
-	@Override
 	public int turnAttack(PriorityQueue<Titan> laneTitans) {
-		// TODO Auto-generated method stub
-		return 0;
+		int resourcesValue = 0;
+		int damage = this.getDamage();
+		int currentTitans = Math.min(laneTitans.size(), 5);
+
+		PriorityQueue<Titan> attackQueue = new PriorityQueue<>(
+				(titan1, titan2) -> Integer.compare(titan1.getDistance(), titan2.getDistance()));
+
+		for (int i = 0; i < currentTitans; i++) {
+			if (laneTitans.isEmpty()) {
+				break;
+			}
+			attackQueue.add(laneTitans.poll());
+		}
+
+		while (!attackQueue.isEmpty()) {
+			Titan targetTitan = attackQueue.poll();
+			targetTitan.takeDamage(damage);
+			if (targetTitan.isDefeated()) {
+				resourcesValue += targetTitan.getResourcesValue();
+			}
+
+		}
+
+		return resourcesValue;
 	}
 
 }
