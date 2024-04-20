0dcf5aeb14a5b6b235a30361bb4416f79a76beda
Omar <omarsherifmohammedomar@gmail.com>
2024-04-20 15:20:57 +0200

    .

--- src/game/engine/weapons/SniperCannon.java
+++ src/game/engine/weapons/SniperCannon.java
@@ -12,11 +12,13 @@ public class SniperCannon extends Weapon {
 	}
 
 	public int turnAttack(PriorityQueue<Titan> laneTitans) {
-		Titan currentTitan = laneTitans.peek();
+		PriorityQueue<Titan> secondQueue = new PriorityQueue<>(
+				(titan1, titan2) -> Integer.compare(titan1.getDistance(), titan2.getDistance()));
+		Titan currentTitan = secondQueue.peek();
 		int damage = this.getDamage();
 		currentTitan.takeDamage(damage);
 		if (currentTitan.isDefeated()) {
-			laneTitans.remove();
+			secondQueue.remove();
 			return currentTitan.getResourcesValue();
 		}
 		return 0;
