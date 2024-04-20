d687dcf85a25a967a9b4fe707e58794ff6647fcf
Omar <omarsherifmohammedomar@gmail.com>
2024-04-20 15:20:07 +0200

    .

--- src/game/engine/weapons/VolleySpreadCannon.java
+++ src/game/engine/weapons/VolleySpreadCannon.java
@@ -5,15 +5,15 @@ import java.util.PriorityQueue;
 import game.engine.titans.Titan;
 
 public class VolleySpreadCannon extends Weapon {
-    public static final int WEAPON_CODE = 3;
-    private final int minRange;
-    private final int maxRange;
+	public static final int WEAPON_CODE = 3;
+	private final int minRange;
+	private final int maxRange;
 
-    public VolleySpreadCannon(int baseDamage, int minRange, int maxRange) {
-        super(baseDamage);
-        this.minRange = minRange;
-        this.maxRange = maxRange;
-    }
+	public VolleySpreadCannon(int baseDamage, int minRange, int maxRange) {
+		super(baseDamage);
+		this.minRange = minRange;
+		this.maxRange = maxRange;
+	}
 
 	public int getMinRange() {
 		return minRange;
@@ -23,13 +23,19 @@ public class VolleySpreadCannon extends Weapon {
 		return maxRange;
 	}
 
-	@Override
 	public int turnAttack(PriorityQueue<Titan> laneTitans) {
-		// TODO Auto-generated method stub
+		PriorityQueue<Titan> secondQueue = new PriorityQueue<>(
+				(titan1, titan2) -> Integer.compare(titan1.getDistance(), titan2.getDistance()));
+		Titan currentTitan = secondQueue.peek();
+		int damage = this.getDamage();
+		if (currentTitan.getDistance() > this.getMinRange() && currentTitan.getDistance() < this.getMaxRange()) {
+			currentTitan.takeDamage(damage);
+		}
+		if (currentTitan.isDefeated()) {
+			secondQueue.remove();
+			return currentTitan.getResourcesValue();
+		}
 		return 0;
 	}
-
-   
-
 
 }
