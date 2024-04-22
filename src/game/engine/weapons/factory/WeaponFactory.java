46844dce78217e63ccdce645010890a364f3807c
Omar <omarsherifmohammedomar@gmail.com>
2024-04-22 13:07:37 +0200

    .

--- src/game/engine/weapons/factory/WeaponFactory.java
+++ src/game/engine/weapons/factory/WeaponFactory.java
@@ -19,41 +19,47 @@ public class WeaponFactory {
 		return weaponShop;
 	}
 
-	
+	public FactoryResponse buyWeapon(int resources, int weaponCode) throws InsufficientResourcesException {
+		if (!weaponShop.containsKey(weaponCode)) {
+			throw new IllegalArgumentException("Invalid weapon code: " + weaponCode);
+		}
 
-    public FactoryResponse buyWeapon(int resources, int weaponCode) throws InsufficientResourcesException {
-        if (!weaponShop.containsKey(weaponCode)) {
-            throw new IllegalArgumentException("Invalid weapon code: " + weaponCode);
-        }
+		WeaponRegistry weaponData = weaponShop.get(weaponCode);
+		int weaponPrice = weaponData.getPrice();
 
-        WeaponRegistry weaponData = weaponShop.get(weaponCode);
-        int weaponPrice = weaponData.getPrice();
+		if (resources < weaponPrice) {
+			throw new InsufficientResourcesException("Insufficient resources to buy weapon", resources);
+		}
 
-        if (resources < weaponPrice) {
-            throw new InsufficientResourcesException("Insufficient resources to buy weapon",resources);
-        }
+		Weapon weapon = weaponData.buildWeapon();
+		int remainingResources = resources - weaponPrice;
 
-        Weapon weapon = weaponData.buildWeapon();
-        int remainingResources = resources - weaponPrice;
+		return new FactoryResponse(weapon, remainingResources);
+	}
 
-        return new FactoryResponse(weapon, remainingResources);
-    }
-    public void addWeaponToShop(int code, int price) {
-        if (weaponShop.containsKey(code)) {
-            throw new IllegalArgumentException("Weapon with code " + code + " already exists in the shop");
-        }
-        weaponShop.put(code, new WeaponRegistry(code, price));
-    }
-    public void addWeaponToShop(int code, int price, int damage, String name) {
-        if (weaponShop.containsKey(code)) {
-            throw new IllegalArgumentException("Weapon with code " + code + " already exists in the shop");
-        }
-        weaponShop.put(code, new WeaponRegistry(code, price, damage, name));
-    }
-    public void addWeaponToShop(int code, int price, int damage, String name, int minRange, int maxRange) {
-        if (weaponShop.containsKey(code)) {
-            throw new IllegalArgumentException("Weapon with code " + code + " already exists in the shop");
-        }
-        weaponShop.put(code, new WeaponRegistry(code, price, damage, name, minRange, maxRange));
-    }
+	public void addWeaponToShop(int code, int price) {
+		if (!weaponShop.containsKey(code)) {
+			weaponShop.put(code, new WeaponRegistry(code, price));
+		} else {
+			throw new IllegalArgumentException("Weapon with code " + code + " already exists in the shop");
+		}
+
+	}
+
+	public void addWeaponToShop(int code, int price, int damage, String name) {
+		if (!weaponShop.containsKey(code)) {
+			weaponShop.put(code, new WeaponRegistry(code, price, damage, name));
+		} else {
+			throw new IllegalArgumentException("Weapon with code " + code + " already exists in the shop");
+		}
+
+	}
+
+	public void addWeaponToShop(int code, int price, int damage, String name, int minRange, int maxRange) {
+		if (!weaponShop.containsKey(code)) {
+			weaponShop.put(code, new WeaponRegistry(code, price, damage, name, minRange, maxRange));
+		} else {
+			throw new IllegalArgumentException("Weapon with code " + code + " already exists in the shop");
+		}
+	}
 }
