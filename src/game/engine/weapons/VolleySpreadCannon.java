018f78a7c0f0124b933fc58d84da9bbdff013130
Omar <omarsherifmohammedomar@gmail.com>
2024-04-23 17:48:46 +0200

    .

--- src/game/engine/weapons/VolleySpreadCannon.java
+++ src/game/engine/weapons/VolleySpreadCannon.java
@@ -37,7 +37,7 @@ public class VolleySpreadCannon extends Weapon {
 				if (titan.isDefeated()) {
 					resourcesValue += titan.getResourcesValue();
 				} else {
-					currentTitans.add(titan);
+					currentTitans.offer(titan);
 				}
 			}
 		}
