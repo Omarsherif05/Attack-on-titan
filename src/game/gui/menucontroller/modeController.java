6801906451b54f5fdb285b8217d9c63b6278cb3b
Omar <omarsherifmohammedomar@gmail.com>
2024-05-18 00:25:42 +0300

    .

--- src/game/gui/menucontroller/modeController.java
+++ src/game/gui/menucontroller/modeController.java
@@ -19,10 +19,12 @@ public class modeController {
 	private Parent root;
 
 	public void easy(ActionEvent e) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("gameplay.fxml"));
+		root = FXMLLoader.load(getClass().getResource("easy.fxml"));
 		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
+		String css = this.getClass().getResource("/game/gui/menucontroller/easy.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		stage.setFullScreen(true);
 		stage.setFullScreenExitHint("");
 		stage.show();
@@ -37,12 +39,14 @@ public class modeController {
 	}
 
 	public void hard(ActionEvent e) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("gameplay.fxml"));
+		root = FXMLLoader.load(getClass().getResource("hard.fxml"));
 		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.setFullScreen(true);
 		stage.setFullScreenExitHint("");
+		String css = this.getClass().getResource("/game/gui/menucontroller/hard.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		stage.show();
 		Battle battle = new Battle(1, 0, 0, 5, 125);
 
